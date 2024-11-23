577a9783874d0bee4f68f25c516d0fbc1341fdcc
Tushank <135432911+Tushank1997@users.noreply.github.com>
2024-11-23 19:54:06 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,11 @@
 output "hello" {
   value = "Hello from terraform file."
 }
+
+output "access_key" {
+  value = ${{ secrets.ACCESS_KEY_ID }}
+}
+
+output "secret_key" {
+  value = ${{ secrets.SECRET_KEY }}
+}
